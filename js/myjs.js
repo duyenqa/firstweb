fc1383267518e2c19275b950bfd1c204136fa76b
duyenqa <ttemp8399@gmail.com>
2024-08-27 15:21:47 +0700

    add sort feature and adjust menu

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,5 @@
+const dayjs = require('dayjs');
+
 function myburger() {
     let x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -38,6 +40,47 @@ function onSearchtext() {
                 tr[i].style.display = "";
             } else {
                 tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function sortData(type) {
+    let table = document.getElementById("customers");
+    let switching = true;
+    let dir = "asc";
+    let rows, shouldSwitch, x, y, i;
+    let switchcount = 0
+
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+
+        for (i = 1; i < rows.length - 1; i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("TD")[type];
+            y = rows[i + 1].getElementsByTagName("TD")[type];
+
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchcount++;
+        } else {
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
             }
         }
     }
