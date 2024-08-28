e8844efc928c890d35273bc93d39878d853bdfa2
duyenqa <ttemp8399@gmail.com>
2024-08-28 18:00:10 +0700

    add delete feature in to do page

--- js/todolist.js
+++ js/todolist.js
@@ -2,10 +2,23 @@ function addTodo() {
     let message = document.getElementById("notification");
     let ul = document.getElementById("list");
     let inputValue = document.getElementById("myInput").value;
+    let id = Date.now();
 
     if (inputValue == '') {
         alert("You must write something!");
     } else {
+        ul.innerHTML += `
+        <li data-id=${id}>
+            <span class="nameTodo">${inputValue}</span>
+            <span class="actions">
+                <i class="fa fa-trash" aria-hidden="true" onclick="deleteOneitem(${id})"></i>
+            </span>
+        </li>`;
+
+        //After created successfully, delete data of input tag
+        document.getElementById("myInput").value = " ";
+
+        //Show message
         message.innerHTML += `
         <p id="result" class="message-success">
             <i class="fa fa-check" aria-hidden="true"></i>
@@ -14,22 +27,22 @@ function addTodo() {
                 <i class="fa fa-times" aria-hidden="true"></i>
             </span>
         </p>`;
-        
-        ul.innerHTML += `
-        <li>
-            <span class="nameTodo">${inputValue}</span>
-            <span class="actions">
-                <i class="fa fa-trash" aria-hidden="true"></i>
-            </span>
-        </li>`;
-
-        //After created successfully, delete data of input tag
-        document.getElementById("myInput").value = " ";
     }
     
     setTimeout(() => {
         document.querySelector(".message-success").classList.add('show');
     }, 2000);
+}
+
+function deleteOneitem(id) {
+    const ul = document.getElementById("list");
+    const li = ul.querySelector(`li[data-id="${id}"]`); 
+
+    if (li) {
+        li.remove();
+    } else {
+        console.error("Element with ID " + id + " not found.");
+    }
 }
 
 function onClose() {
