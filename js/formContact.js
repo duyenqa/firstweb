c791caa399a630be952c0718718c74d1a042698f
duyenqa <ttemp8399@gmail.com>
2024-11-16 19:25:08 +0700

    update contact page

--- js/formContact.js
+++ js/formContact.js
@@ -5,7 +5,10 @@ function showAlert() {
     let description = document.getElementById("description").value;
     let gender = "";
     let genderRadioButtons = document.getElementsByName("gender");
+    let colors = "";
+    let colorCheckboxs = document.getElementsByName("color");
 
+    //radios
     for (let i = 0; i < genderRadioButtons.length; i++) {
         if (genderRadioButtons[i].checked) {
             gender = genderRadioButtons[i].value;
@@ -13,7 +16,14 @@ function showAlert() {
         }
     }
 
-    let message = "Full Name: " + lastName + " " + firstName + "\nGender: " + gender + "\nLive in: " + country + "\nDescription: " + description;
+    //checkboxs
+    for (let i = 0; i < colorCheckboxs.length; i++) {
+        if (colorCheckboxs[i].checked) {
+            colors += colorCheckboxs[i].value + " ";
+        }
+    }
+
+    let message = "Full Name: " + lastName + " " + firstName + "\nGender: " + gender  + "\nLive in: " + country + "\nDescription: " + description + "\nColors: " + colors;
 
     confirm(message);
 }
