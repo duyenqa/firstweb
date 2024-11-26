e2f7e11ab759cf1c4fbda935879545d79644add2
duyenqa <ttemp8399@gmail.com>
2024-11-26 19:55:29 +0700

    update index page

--- js/ItemProduct.js
+++ js/ItemProduct.js
@@ -1,5 +1,7 @@
 const productList = document.getElementById('product-list');
-productList.innerHTML = '<button class="buttonload"><i class="fa fa-circle-o-notch fa-spin"></i>&nbsp;Loading Product...</button>';
+const productSearch = document.getElementById('productSearch');
+
+productList.innerHTML = '<button class="buttonload"><i class="fa fa-circle-o-notch fa-spin"></i>&nbsp;Loading Products...</button>';
 
 axios.get('https://fakestoreapi.com/products')
     .then(response => {
@@ -10,47 +12,64 @@ axios.get('https://fakestoreapi.com/products')
             return;
         }
 
-        const shuffledProducts = products .map(product => ({ product, sort: Math.random() })) .sort((a, b) => a.sort - b.sort) .map(({ product }) => product);
-
-        const productsHtml = shuffledProducts.map(product => {
-            if (product) {
+        const displayProducts = (filteredProducts) => {
+            const productsHtml = filteredProducts.map(product => {
                 return `
-                    <div class="box">
-                        <div class="wrap-img">
-                            <img class="pic" src="${product.image}" alt="${product.title}">
+                        <div class="box">
+                            <div class="wrap-img">
+                                <img class="pic" src="${product.image}" alt="${product.title}">
+                            </div>
+                            <div class="product-details">
+                                <h4 class="name-product">${product.title}</h4>
+                                <p class="type" style="display: none;">${product.category}</p>
+                                <p class="info" style="display: none;">${product.description}</p>
+                                <p class="price">$${product.price}</p>
+                            </div>
+                            <div class="buttons">
+                                <button class="buy-button">Detail</button>
+                                <button class="cart-button">Add Cart</button>
+                            </div>
                         </div>
-                        <div class="product-details">
-                            <h4 class="name-product">${product.title}</h4>
-                            <p class="type" style="display: none;">${product.category}</p>
-                            <p class="info" style="display: none;">${product.description}</p>
-                            <p class="price">$${product.price}</p>
-                        </div>
-                        <div class="buttons">
-                            <button class="buy-button">Detail</button>
-                            <button class="cart-button">Add Cart</button>
-                        </div>
-                    </div>
-                `;
-            } else {
-                return '<p>Product is unavailable.</p>';
-            }
-        }).join('');
+                    `;
+            }).join('');
 
-        productList.innerHTML = productsHtml;
+            productList.innerHTML = productsHtml;
 
-        const buyButtons = document.querySelectorAll('.buy-button');
-        const productItem = document.getElementById('mySidenav');
-        buyButtons.forEach(button => {
-            button.addEventListener('click', () => {
-                productItem.style.width = "280px";
-                const productTitle = button.closest('.box').querySelector('.name-product').textContent;
-                const productDescription = button.closest('.box').querySelector('.info').textContent;
-                const productCategory = button.closest('.box').querySelector('.type').textContent;
+            const buyButtons = document.querySelectorAll('.buy-button');
+            const productItem = document.getElementById('mySidenav');
+            buyButtons.forEach(button => {
+                button.addEventListener('click', () => {
+                    productItem.style.width = "280px";
+                    const productTitle = button.closest('.box').querySelector('.name-product').textContent;
+                    const productDescription = button.closest('.box').querySelector('.info').textContent;
+                    const productCategory = button.closest('.box').querySelector('.type').textContent;
 
-                document.getElementById("nameProduct").innerHTML = `${productTitle}`
-                document.getElementById("detailProduct").innerHTML = `${productDescription}`
-                document.getElementById("typeCategory").innerHTML = `${productCategory}`
+                    document.getElementById("nameProduct").innerHTML = `${productTitle}`;
+                    document.getElementById("detailProduct").innerHTML = `${productDescription}`;
+                    document.getElementById("typeCategory").innerHTML = `${productCategory}`;
+                });
             });
+        };
+
+        displayProducts(products);
+
+        productSearch.addEventListener('input', () => {
+            const searchQuery = productSearch.value.toLowerCase();
+
+            if (!searchQuery) {
+                displayProducts(products);
+                return;
+            }
+
+            const filteredProducts = products.filter(item =>
+                item.title.toLowerCase().includes(searchQuery)
+            );
+
+            if (filteredProducts.length === 0) {
+                productList.innerHTML = '<p>No matching products found.</p>';
+                return;
+            }
+            displayProducts(filteredProducts);
         });
     })
     .catch(error => {
