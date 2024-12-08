9c8c2e2e2a33f7ac361098d12be58d9d4086159e
duyenqa <ttemp8399@gmail.com>
2024-12-09 00:12:37 +0700

    upadate index page

--- js/popupsale.js
+++ js/popupsale.js
@@ -4,10 +4,10 @@ const closePopUp = document.getElementById("closeSale");
 window.onload = function () {
     setTimeout(function () {
         openPopUpSale.style.display = "block";
+        openPopUpSale.style.width = "100%";
         Notification.requestPermission().then(permission => {
             alert(permission);
         });
-
     }, 2000)
 
     closePopUp.addEventListener("click", () => {
