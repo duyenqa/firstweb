72089487f12d3c7ab471c0f2f36b2b61292f5182
duyenqa <ttemp8399@gmail.com>
2024-12-12 10:48:34 +0700

    update log out in dashboard page

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,19 @@
 function onLogOut() {
-    // Clear the login status from localStorage
-    localStorage.removeItem('isLoggedIn');
+    document.getElementById("popupLogout").style.display = "block";
+    
+    document.getElementById('close').addEventListener("click", () =>{
+        document.getElementById("popupLogout").style.display = "none";
+    })
 
-    // Redirect to login page
-    window.location.href = "login.html";
+    document.getElementById('closebtn').addEventListener("click", () =>{
+        document.getElementById("popupLogout").style.display = "none";
+    })
+
+    document.getElementById('okbtn').addEventListener("click", () =>{
+        // Clear the login status from localStorage
+        localStorage.removeItem('isLoggedIn');
+
+        // Redirect to login page
+        window.location.href = "login.html";
+    })
 }
