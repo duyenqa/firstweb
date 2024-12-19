2ee7c075ebbd72045a1a2b9a4ed11b4759e0653f
duyenqa <ttemp8399@gmail.com>
2024-12-19 22:31:03 +0700

    add call api provinces in cart pagepage

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Call API:
 - [Products - Home Page](https://fakestoreapi.com/products)
 - [Authors - Contact Page](https://jsonplaceholder.typicode.com/users)
+- [Provinces - Cart Page](https://provinces.open-api.vn/api/)
 
 ## Author:
 Ngô Thị Kim Duyên
---
+++ js/dropdownprovinces.js
@@ -0,0 +1,13 @@
+const provinceList = document.getElementById('province');
+
+axios.get('https://provinces.open-api.vn/api/')
+    .then(response => {
+        const provincesHtml = response.data.map(item => {
+            return `<option value=${item.code}>${item.name}</option>`
+        }).join('');
+
+        provinceList.innerHTML = provincesHtml;
+    }).catch(error => {
+        console.error('Error fetching data:', error);
+        productList.innerHTML = '<p>Unable to load provincesprovinces at the moment. Please try again later.</p>';
+    });
