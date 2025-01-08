ae96d500dc900f55ce66c93228342b1894254c37
duyenqa <ttemp8399@gmail.com>
2025-01-08 15:57:19 +0700

    update all icons

--- js/ItemProduct.js
+++ js/ItemProduct.js
@@ -5,7 +5,7 @@ const quantity = document.getElementById("qtyCart");
 let carts = JSON.parse(localStorage.getItem('cart')) || [];
 let hearts = JSON.parse(localStorage.getItem('likes')) || [];
 
-productList.innerHTML = '<button class="buttonload"><i class="fa fa-circle-o-notch fa-spin"></i>&nbsp;Loading Products...</button>';
+productList.innerHTML = '<button class="buttonload"><i class="fa-solid fa-spinner fa-spin"></i>&nbsp;Loading Products...</button>';
 
 axios.get('https://fakestoreapi.com/products')
     .then(response => {
@@ -40,7 +40,7 @@ axios.get('https://fakestoreapi.com/products')
                 return `
                         <div class="box">
                             <button class="likeItem" data-id="${product.id}">
-                                <i class="fa fa-heart-o" aria-hidden="true"></i>
+                                <i class="fa-regular fa-heart"></i>
                             </button>
                             <div class="wrap-img">
                                 <img class="pic" src="${product.image}" alt="${product.title}" loading="lazy" />
@@ -67,16 +67,16 @@ axios.get('https://fakestoreapi.com/products')
                     const icon = button.querySelector('i');
                     const productId = button.getAttribute('data-id');
                     
-                    if (icon.classList.contains('fa-heart-o')) {
-                        icon.classList.remove('fa-heart-o');
-                        icon.classList.add('fa-heart');
+                    if (icon.classList.contains('fa-regular')) {
+                        icon.classList.remove('fa-regular');
+                        icon.classList.add('fa-solid');
                         icon.style.color = 'red';
 
                         hearts.push(productId);
                         console.log("heart list: ", hearts);
                     } else {
-                        icon.classList.remove('fa-heart');
-                        icon.classList.add('fa-heart-o');
+                        icon.classList.remove('fa-solid');
+                        icon.classList.add('fa-regular');
                         icon.style.color = '';
 
                         hearts = hearts.filter(id => id !== productId);
