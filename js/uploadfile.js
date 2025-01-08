66878207071337cc4628ae68067c605e33ea5f91
duyenqa <ttemp8399@gmail.com>
2025-01-08 22:50:13 +0700

    update gallery images

--- js/uploadfile.js
+++ js/uploadfile.js
@@ -7,17 +7,19 @@ function preview() {
     numOfFiles.textContent = `${fileInput.files.length} Files Selected`;
     for(i of fileInput.files){
         let reader = new FileReader();
-        let figure = document.createElement("figure");
-        let figCap = document.createElement("figcaption");
-
+        let aTag = document.createElement("a");
+        let figCap = document.createElement("div");
+        figCap.classList.add("caption");
         figCap.innerText = i.name;
-        figure.appendChild(figCap);
+        aTag.appendChild(figCap);
         reader.onload = () => {
             let img = document.createElement("img");
             img.setAttribute("src", reader.result);
-            figure.insertBefore(img, figCap);
+            aTag.setAttribute("target", "_blank");
+            aTag.setAttribute("href", reader.result);
+            aTag.insertBefore(img, figCap);
         }
-        imageContainer.appendChild(figure);
+        imageContainer.appendChild(aTag);
         reader.readAsDataURL(i);
     }
 }
