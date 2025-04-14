d611f46323e7cbbbf261815b76b92f937da00042
duyenqa <ttemp8399@gmail.com>
2025-04-14 13:18:33 +0700

    update drag and drop page

--- js/draganddrop.js
+++ js/draganddrop.js
@@ -1,48 +1,13 @@
-const item = document.querySelector('.item');
-
-item.addEventListener('dragstart', dragStart);
-
-function dragStart(e) {
-    e.dataTransfer.setData('text/plain', e.target.id);
-    setTimeout(() => {
-        e.target.classList.add('hide');
-    }, 0);
+function allowDrop(event) {
+    event.preventDefault();
 }
 
-const boxes = document.querySelectorAll('.box');
-
-boxes.forEach(box => {
-    box.addEventListener('dragenter', dragEnter)
-    box.addEventListener('dragover', dragOver);
-    box.addEventListener('dragleave', dragLeave);
-    box.addEventListener('drop', drop);
-});
-
-
-function dragEnter(e) {
-    e.preventDefault();
-    e.target.classList.add('drag-over');
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
 }
 
-function dragOver(e) {
-    e.preventDefault();
-    e.target.classList.add('drag-over');
-}
-
-function dragLeave(e) {
-    e.target.classList.remove('drag-over');
-}
-
-function drop(e) {
-    e.target.classList.remove('drag-over');
-
-    // get the draggable element
-    const id = e.dataTransfer.getData('text/plain');
-    const draggable = document.getElementById(id);
-
-    // add it to the drop target
-    e.target.appendChild(draggable);
-
-    // display the draggable element
-    draggable.classList.remove('hide');
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
 }
